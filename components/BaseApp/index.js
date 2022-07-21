3f10a32586d1cc2d729597dc2ddb883b2cd0977d
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2022-07-22 02:05:57 +0530

    search and display frame of video functionality added

--- components/BaseApp/index.js
+++ components/BaseApp/index.js
@@ -1,9 +1,18 @@
-import React from "react";
+import { SearchIcon } from "@heroicons/react/outline";
+import React, { useState } from "react";
 import Draggable from "react-draggable";
+import { extractVideoID } from "../globalFunctions/extractVideoID";
 import { toggleApp } from "../globalFunctions/toggle";
 
 export default function BaseApp() {
   const nodeRef = React.useRef(null);
+  const [url, setUrl] = useState("");
+  const [id, setId] = useState("zp-ZXtTcobw");
+  function handleForm(e) {
+    e.preventDefault();
+    setId(extractVideoID(url));
+    setUrl("");
+  }
 
   return (
     <>
@@ -28,13 +37,37 @@ export default function BaseApp() {
               }}
             ></button>
           </div>
+          <form
+            action=""
+            onSubmit={handleForm}
+            className="transition-all w-[96%] text-xl relative mx-auto my-2"
+          >
+            <input
+              type="url"
+              name="url-box"
+              value={url}
+              onChange={(e) => setUrl(e.target.value)}
+              id="urlBox"
+              autoFocus={true}
+              autoComplete="off"
+              className="transition-all bg-slate-100  outline-none border-2 border-transparent rounded-xl text-xl focus:border-slate-500 focus:scale-[103%] p-2 w-full"
+              placeholder=" Video URL"
+              required={true}
+            />
+            <button
+              type="submit "
+              className="absolute right-1 top-1 w-10 h-10 z-10 bg-slate-100 rounded-full"
+            >
+              <SearchIcon className="w-full stroke-slate-600 dark:stroke-slate-300 stroke-[1]" />
+            </button>
+          </form>
           <div className="flex flex-col items-center align-center">
             {/* play any other song property search-bar */}
             <iframe
               width="560"
               height="315"
               className="w-full rounded"
-              src="https://www.youtube.com/embed/zp-ZXtTcobw"
+              src={`https://www.youtube.com/embed/${id}`}
               title="YouTube video player"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
