c4cc6e31a89648c332353a9564f86cca92ed36a3
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2022-07-22 00:36:48 +0530

    basic fixes

--- components/BaseApp/index.js
+++ components/BaseApp/index.js
@@ -1,4 +1,3 @@
-import { CogIcon, PlusCircleIcon } from "@heroicons/react/outline";
 import React from "react";
 import Draggable from "react-draggable";
 import { toggleApp } from "../globalFunctions/toggle";
@@ -13,28 +12,21 @@ export default function BaseApp() {
           className={
             "w-[85%] sm:w-96 bg-fuchsia-100/60 mx-auto ring-2 rounded-md ring-fuchsia-100/80 backdrop-blur-sm relative z-10 mt-6"
           }
-          id="music"
           ref={nodeRef}
+          id="music"
         >
           <div
-            className="w-full flex flex-row text-base items-center justify-between p-[0.1rem] bg-fuchsia-100/60 rounded-t-md cursor-pointer"
+            className="w-full flex flex-row text-base items-center justify-between p-[0.1rem] bg-fuchsia-100/60 rounded-t-md cursor-pointer px-1"
             id="draggableDiv"
           >
             <div className="text-lg">Music</div>
-            <div className="flex flex-row gap-1">
-              <button className="w-6 text-fuchsia-700 ">
-                <CogIcon className="w-full h-full" />
-              </button>
 
-              <button
-                className="w-6 text-red-500 rotate-45"
-                onClick={() => {
-                  toggleApp("music");
-                }}
-              >
-                <PlusCircleIcon className="w-full h-full" />
-              </button>
-            </div>
+            <button
+              className="w-5 h-5 bg-pink-400 rounded-full"
+              onClick={() => {
+                toggleApp("music");
+              }}
+            ></button>
           </div>
           <div className="flex flex-col items-center align-center">
             {/* play any other song property search-bar */}
